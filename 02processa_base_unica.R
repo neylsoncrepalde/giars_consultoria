2518e29481a617d4d07b16c687867493784e577c
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-03-31 00:54:50 -0300

    script 2 going

---
+++ 02processa_base_unica.R
@@ -0,0 +1,11 @@
+####################################
+### PESCARTE
+### GIARS - UFMG
+### Script: Neylson Crepalde
+### Objetivo: Processa a base única
+####################################
+
+library(readr)
+library(dplyr)
+
+dados = read_csv('pescarte_nova.csv')
