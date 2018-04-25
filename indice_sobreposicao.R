f62fd82cbdefb76333d55e8f7a21f7cb585666b7
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-04-25 13:31:29 -0300

    prepara as matrizes

--- indice_sobreposicao.R
+++ indice_sobreposicao.R
@@ -7,3 +7,24 @@
 
 source('soma_matrizes.R')
 
+# Extraindo as matrizes binárias das questões de 1 a 4
+M4_1 = as.matrix(get.adjacency(Q4_1))
+M4_2 = as.matrix(get.adjacency(Q4_2))
+M4_3 = as.matrix(get.adjacency(Q4_3))
+M4_4 = as.matrix(get.adjacency(Q4_4))
+M4_5 = as.matrix(get.adjacency(Q4_5))
+
+# Testando se são binárias
+which(M4_1 > 1) # matriz binária
+which(M4_2 > 1) # matriz binária
+which(M4_3 > 1) # matriz binária
+which(M4_4 > 1) # matriz binária
+which(M4_5 > 1) # matriz binária
+
+#########################################
+# Produzindo o índice com a seguinte ideia:
+# Laços sobrepostos (soma = 2) sobre laços existentes (soma > 0)
+#----------------------------------
+# Somando as duas primeiras matrizes
+
+
